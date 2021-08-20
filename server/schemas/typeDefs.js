1fa2d9102e60d40905b1084c5f3d2e3016315944
Chuck Fields <info@spacetechcorp.com>
2021-08-20 11:42:09 -0400

    Added mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,11 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
   type User {
     _id: ID
     username: String
@@ -28,10 +33,19 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+      login(email: String!, password: String!): Auth
+      addUser(username: String!, email: String!, password: String!): Auth
+      addThought(thoughtText: String!): Thought
+      addReaction(thoughtId: ID!, reactionBody: String!): Thought
+      addFriend(friendId: ID!): User
   }
 `;
 
